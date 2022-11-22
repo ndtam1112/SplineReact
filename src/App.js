b01ba6fa5557512120f55725e1fb2a606e8a8973
ndtam1112 <ndtam1010@gmail.com>
2022-11-23 03:52:20 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import Spline from '@splinetool/react-spline';
 
-import React from 'react'
-
-const App = () => {
+export default function App() {
   return (
-    <div>
     <Spline scene="https://prod.spline.design/CpraBEimqdJYJwMw/scene.splinecode" />
-    </div>
-  )
+  );
 }
-
-export default App
